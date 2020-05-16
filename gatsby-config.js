09035d2b4a4c093e4251f80d7e4218b5af15416d
nisarhassan12 <syednisarhassan12@gmail.com>
2020-05-16 09:35:13 +0500

    basic setup.

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+const title = "Nisar Hassan Naqvi | A web developer & interface designer."
+const description = "Website and Blog of Nisar Hassan Naqvi."
+const keywords = [
+    "Nisar Hassan Naqvi",
+    "Web Developer",
+    "Interface Designer"
+]
+
+const plugins = [
+    'gatsby-plugin-typescript'
+]
+
+module.exports = {
+    siteMetadata: {
+        title,
+        description,
+        keywords
+    },
+    plugins
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => (
+    <div>
+        <h1>Think before you speak.</h1>
+    </div>
+)
+
+export default Index
